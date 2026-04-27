23370a60557ab291a3bee75500c8575cd057f843
13e404 <85143913+13e404@users.noreply.github.com>
2026-04-27 14:35:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import sqlite3
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -12,9 +14,11 @@ def init_db():
         CREATE TABLE IF NOT EXISTS items (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             category TEXT,
-            project TEXT,
-            qty TEXT,
-            status TEXT
+            project_number TEXT,
+            size TEXT,
+            color TEXT,
+            quantity TEXT,
+            date TEXT
         )
     ''')
 
@@ -35,23 +39,32 @@ def category_page(category):
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
 
+    # اضافه کردن دیتا
     if request.method == 'POST':
-        project = request.form['project']
-        qty = request.form['qty']
-        status = request.form['status']
+        project_number = request.form['project_number']
+        size = request.form['size']
+        color = request.form['color']
+        quantity = request.form['quantity']
 
-        c.execute(
-            "INSERT INTO items (category, project, qty, status) VALUES (?, ?, ?, ?)",
-            (category, project, qty, status)
-        )
+        date = datetime.now().strftime("%Y-%m-%d")
+
+        c.execute("""
+            INSERT INTO items (category, project_number, size, color, quantity, date)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (category, project_number, size, color, quantity, date))
+
         conn.commit()
 
+    # گرفتن دیتاها
     c.execute("SELECT * FROM items WHERE category=?", (category,))
     rows = c.fetchall()
+
     conn.close()
 
     return render_template('table.html', category=category, rows=rows)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+# ---------------- RUN SERVER ----------------
+if name == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
